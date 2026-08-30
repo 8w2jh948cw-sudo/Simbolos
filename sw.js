8794142fb80b7ba624e10ece1032f2daedf588a3
8w2jh948cw-sudo <8w2jh948cw@privaterelay.appleid.com>
2026-08-30 19:49:48 -0300

    Make PWA updates activate immediately

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = "simbolos-v5";
+const CACHE = "simbolos-v6";
 const ASSETS = ["./", "./index.html", "./styles.css", "./variants.css", "./intelligence.css", "./app.js", "./enhancements.js", "./variant-intelligence.js", "./manifest.webmanifest", "./icon.svg"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
